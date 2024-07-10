54317fed64b4b25e027d422fbf5557cfd87bd48b
Matt Wilson <matthew.wilson.synesis@gmail.com>
2024-07-10 15:14:50 +1000

    ~ boilerplate files enhanced functionality

--- build_cmake.sh
+++ build_cmake.sh
@@ -5,14 +5,48 @@ Dir=$(cd $(dirname "$ScriptPath"); pwd)
 Basename=$(basename "$ScriptPath")
 CMakeDir=$Dir/_build
 
+IgnoreRemainingFlagsAndOptions=0
+Targets=()
+
+
+# ##########################################################
+# functions
+
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
 
 # ##########################################################
 # command-line handling
 
 while [[ $# -gt 0 ]]; do
-    case $1 in
-        --help)
-            cat << EOF
+
+  if [ $IgnoreRemainingFlagsAndOptions -ne 0 ]; then
+
+    Targets+=($1)
+
+    shift
+
+    continue
+  else
+
+    if [ ! ${1:0:1} = '-' ]; then
+
+      Targets+=($1)
+
+      shift
+
+      continue
+    fi
+  fi
+
+  case $1 in
+    --)
+
+      IgnoreRemainingFlagsAndOptions=1
+      ;;
+    --help)
+
+      cat << EOF
 b64 is a small and simple standalone C-language library that provides Base-64 encoding and decoding
 Copyright (c) 2019-2024, Matthew Wilson and Synesis Information Systems
 Copyright (c) 2004-2019, Matthew Wilson and Synesis Software
@@ -32,16 +66,17 @@ Flags/options:
 
 EOF
 
-            exit 0
-            ;;
-        *)
-            >&2 echo "$ScriptPath: unrecognised argument '$1'; use --help for usage"
+      exit 0
+      ;;
+    *)
 
-            exit 1
-            ;;
-    esac
+      >&2 echo "$ScriptPath: unrecognised argument '$1'; use --help for usage"
 
-    shift
+      exit 1
+      ;;
+  esac
+
+  shift
 done
 
 
@@ -50,30 +85,35 @@ done
 
 if [ ! -d "$CMakeDir" ]; then
 
-    >&2 echo "$ScriptPath: CMake build directory '$CMakeDir' not found so nothing to do; use script 'prepare_cmake.sh' if you wish to prepare CMake artefacts"
+  >&2 echo "$ScriptPath: CMake build directory '$CMakeDir' not found so nothing to do; use script 'prepare_cmake.sh' if you wish to prepare CMake artefacts"
 
-    exit 1
+  exit 1
 else
 
-    cd $CMakeDir
+  cd $CMakeDir
 
-    if [ ! -f "$CMakeDir/Makefile" ]; then
+  if [ ! -f "$CMakeDir/Makefile" ]; then
 
-        >&2 echo "$ScriptPath: CMake build directory '$CMakeDir' does not contain expected file 'Makefile', so a clean cannot be performed. It is recommended that you remove all CMake artefacts using script 'remove_cmake_artefacts.sh' followed by regeneration via 'prepare_cmake.sh'"
+    >&2 echo "$ScriptPath: CMake build directory '$CMakeDir' does not contain expected file 'Makefile', so a clean cannot be performed. It is recommended that you remove all CMake artefacts using script 'remove_cmake_artefacts.sh' followed by regeneration via 'prepare_cmake.sh'"
 
-        exit 1
+    exit 1
+  else
+
+    if [ -z "$Targets" ]; then
+
+      echo "Executing build (via command \`make\`)"
     else
 
-        echo "Executing build (via command \`make\`)"
-
-        make
-
-        status=$?
-
-        cd ->/dev/null
-
-        exit $status
+      echo "Executing build (via command \`make\`) with specific target(s) $(join_by , "${Targets[@]}")"
     fi
+
+    make ${Targets[*]}
+    status=$?
+
+    cd ->/dev/null
+
+    exit $status
+  fi
 fi
 
 
